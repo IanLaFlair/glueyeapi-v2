5dba5e4eb8da095b3e2ff82053b9e5d34f41bc00
Alif Nur Rachman <alifnurrachman907@gmail.com>
2021-09-13 22:52:25 +0700

    Create Model

--- app/Models/BloodSugar.php
+++ app/Models/BloodSugar.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model {
+class BloodSugar extends Model
+{
     use HasFactory;
     protected $connection = 'mysql';
-    protected $table = 'user';
+    protected $table = 'blood_sugar';
 }
--- app/Models/Tips.php
+++ app/Models/Tips.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model {
+class Tips extends Model
+{
     use HasFactory;
     protected $connection = 'mysql';
-    protected $table = 'user';
+    protected $table = 'tips';
 }
