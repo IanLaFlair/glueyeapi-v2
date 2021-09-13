ce2ccde2865342f668c0e856969b89f1b40a3baa
Alif Nur Rachman <alifnurrachman907@gmail.com>
2021-09-14 00:18:08 +0700

    Create Middleware for Auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'isOpen' => \App\Http\Middleware\IsOpen::class,
+        'isWeb' => \App\Http\Middleware\IsWeb::class,
     ];
 }
