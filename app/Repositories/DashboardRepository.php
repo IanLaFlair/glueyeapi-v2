ed77b69c85c1f3fa0d494c00f884e6f9d26c94f6
Alif Nur Rachman <alifnurrachman907@gmail.com>
2021-09-13 23:58:45 +0700

    Create Repository Pattern

---
+++ app/Repositories/DashboardRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BloodSugar;
+use App\Models\User;
+use App\Models\Tips;
+
+class DashboardRepository
+{
+    public function get_total_user($params = []) {
+
+    }
+
+    public function get_total_image($params = []) {
+
+    }
+
+    public function get_total_tips($params = []) {
+
+    }
+
+    public function get_total_data_month($params = []) {
+
+    }
+}
---
+++ app/Repositories/TipsRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Tips;
+
+class TipsRepository {
+    public function get($params = []) {
+
+    }
+
+    public function list($params = []) {
+
+    }
+
+    public function store($params = []) {
+
+    }
+
+    public function delete($params = []) {
+
+    }
+}
