c44c33f867e1303794ee965a64aeeed03aebde3e
Alif Nur Rachman <alifnurrachman907@gmail.com>
2021-09-14 22:25:34 +0700

    Create Migrations

--- database/migrations/2021_09_14_152158_create_table_blood_sugar.php
+++ database/migrations/2021_09_14_152158_create_table_blood_sugar.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTableBloodSugar extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('blood_sugar', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->float('prediction_above');
+            $table->float('prediction_below');
+            $table->float('sugar_value');
+            $table->dateTime('date_check');
+            $table->longText('image');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('blood_sugar');
     }
 }
